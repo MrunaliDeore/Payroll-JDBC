c1615d1fc97b21485b7a77b07a83b8e4fa6de7f6
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-17 14:32:18 +0530

    Added : added simple msg in tht class

---
+++ MainClass.java
@@ -0,0 +1,10 @@
+package JDBC;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Assignment no - 34\n");
+		System.out.println("PayRoll program with JDBC");
+	}
+
+}
